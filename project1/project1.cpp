9e9604770fa3870a031e0138e0b71a7d432daef9
U6041109 <U6041109@U6041109-TPL-A>
2017-01-28 14:05:31 -0600

    Add project files.

---
+++ project1/project1.cpp
@@ -0,0 +1,30 @@
+// project1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+	double hours, rate, pay;
+
+	// Get the number of hours worked.
+	cout << "How many hours did you work? ";
+	cin >> hours;
+	
+	// Get the hourly pay rate.
+	cout << "How much do you get paid per hour? ";
+	cin >> rate;
+	
+	// Calculate the pay.
+	pay = hours * rate;
+
+	// Display the pay.
+	cout << "You have earned $" << pay << endl;
+	
+	return 0;
+}
+
